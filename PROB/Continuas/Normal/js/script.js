2cb8181b42aae1ef2fc3f7e1b247ef5984c34260
santiagoHV <santiherreravelas@gmail.com>
2021-02-16 06:04:24 -0500

    avances en distribucion normal

---
+++ PROB/Continuas/Normal/js/script.js
@@ -0,0 +1,8 @@
+let inputMu = document.getElementById("valueMu")
+let inputSigma = document.getElementById("valueSigma")
+let inputX = document.getElementById("valueX")
+
+import "normal-distribution"
+import NormalDistribution from "normal-distribution"
+
+let ola = new NormalDistribution(5, 2)
